7587fafaeac8d1d97a25c909c1a26ee4bcb2559f
Empty <Empty@DESKTOP-PS9H8OG>
2020-05-23 11:22:31 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/cold snack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class holzak : Form
+    {
+        public holzak()
+        {
+            InitializeComponent();
+        }
+
+        private void holzak_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            menu mene = new menu();
+            mene.Show();
+            Hide();
+        }
+    }
+}
---
+++ WindowsFormsApp1/menu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class menu : Form
+    {
+        public menu()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Newrep neww = new Newrep();
+            neww.Show();
+            Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            holzak cold = new holzak();
+            cold.Show();
+            Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hot hot = new hot();
+            hot.Show();
+            Hide();
+                 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dessert des = new dessert();
+            des.Show();
+            Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
